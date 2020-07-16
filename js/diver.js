298a1d7b82dbf27df803359bd7d70224705ffab7
Laura Bonavent <laura.bonavent@gmail.com>
2020-07-16 20:56:22 +0200

    files reorganised

---
+++ js/diver.js
@@ -0,0 +1,27 @@
+// Create the class Diver 
+
+class Diver {
+  constructor(x,y){
+    // create a new element in the html for the image 
+    const img = document.createElement('img')
+    // determine what's happening when the image is loaded 
+    diverImg.onload = () => {
+      this.img = img;
+      // set width, height, x and y
+
+    }
+    // set src of the image (img.src= "")
+  }
+
+  // method to draw the diver 
+  draw() {
+    // if not loaded or of it's loaded 
+  }
+  
+  // method to go up 
+  moveUp() {
+    // add x 
+    // hover on the leg 
+    // bubles in the sea 
+  }
+}
