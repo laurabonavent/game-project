7766324cd363c651e98fd8b76b5d850766e5ef9a
Laura Bonavent <laura.bonavent@gmail.com>
2020-07-19 23:19:29 +0200

    background scroll ok

--- js/game.js
+++ js/game.js
@@ -13,8 +13,7 @@ function background() {
   img.width = W;
   img.height = H;
   var speed = 25; // plus elle est basse, plus c'est rapide
-  var scale = 1.05;
-  var y = -4.5; // décalage vertical
+  var y = 0;
 
   // Programme principal
 
@@ -24,11 +23,10 @@ function background() {
   var x = 0;
   var clearX;
   var clearY;
-  var ctx;
 
   img.onload = function() {
-      imgW = img.width * scale;
-      imgH = img.height * scale;
+      imgW = img.width;
+      imgH = img.height; 
       
       if (imgW > W) {
           x = W - imgW;
@@ -43,9 +41,6 @@ function background() {
       } else {
           clearY = H;
       }
-      
-      // récupérer le contexte du canvas
-      ctx = document.getElementById('canvas').getContext('2d');
   
       // définir le taux de rafraichissement
       return setInterval(draw, speed);
@@ -57,13 +52,14 @@ function background() {
       // si image est <= taille du canvas
       if (imgW <= W) {
           // réinitialise, repart du début
-          if (x > W) {
-              x = -imgW + x;
+          if (x < W) {
+              x = x + imgW;
           }
           // dessine image1 supplémentaire
           if (x > 0) {
               ctx.drawImage(img, -imgW + x, y, imgW, imgH);
           }
+          
           // dessine image2 supplémentaire
           if (x - imgW > 0) {
               ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
@@ -80,50 +76,13 @@ function background() {
           if (x > (W-imgW)) {
               ctx.drawImage(img, x - imgW + 1, y, imgW, imgH);
           }
-      }
+      } 
       // dessine image
       ctx.drawImage(img, x, y,imgW, imgH);
       // quantité à déplacer
-      x += dx;
+      x -= dx;
   }
-  }
-
-
-    function draw() {
-        ctx.clearRect(0, 0, clearX, clearY); // clear the canvas
-        
-        // si image est <= taille du canvas
-        if (imgW <= W) {
-            // réinitialise, repart du début
-            if (x > W) {
-                x = -imgW + x;
-            }
-            // dessine image1 supplémentaire
-            if (x > 0) {
-                ctx.drawImage(img, -imgW + x, y, imgW, imgH);
-            }
-            // dessine image2 supplémentaire
-            if (x - imgW > 0) {
-                ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
-            }
-        }
-
-        // image est > taille du canvas
-        else {
-            // réinitialise, repeart du début
-            if (x > (W)) {
-                x = W - imgW;
-            }
-            // dessine image supplémentaire
-            if (x > (W-imgW)) {
-                ctx.drawImage(img, x - imgW + 1, y, imgW, imgH);
-            }
-        }
-        // dessine image
-        ctx.drawImage(img, x, y,imgW, imgH);
-        // quantité à déplacer
-        x += dx;
-  }
+}
 
 
 function startGame() {
